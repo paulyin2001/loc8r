e7cb95230f66d5446ddc61218755a85e79c8e2ab
Shih-Chun Yin <scyin0121@gmail.com>
2016-03-15 18:19:26 -0400

    Add new controllers and template
    Add locations, others controllers. Update route for new controllers.
    Update layout template with bootstrap navbar and footer

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,12 @@
+/* GET 'home' page */
+module.exports.homelist = function(req,res){
+	res.render('index', { title: 'Home' });
+}
+/* Get 'Location info' page */
+module.exports.locationInfo = function(req,res){
+	res.render('index', { title: 'Location info'});
+}
+/* Get 'Add review' page */
+module.exports.addReview = function(req,res){
+	res.render('index', { title: 'Add review'});
+}
