07469a7e416073c07ffe2cee8513cb475aa4780f
Shih-Chun Yin <scyin0121@gmail.com>
2016-03-16 13:01:08 -0400

    Remove static data in view. Move logic out of view
    Remove static data in view and move to controller. Use mixin in jade to
    make jade javascript external.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,32 @@
 /* GET 'home' page */
 module.exports.homelist = function(req,res){
-	res.render('locations-list', { title: 'Home' });
+	res.render('locations-list', { 
+		title: 'Loc8r - find a place to work with wifi',
+		pageHeader: {
+			title: 'Loc8r',
+			strapline: 'Find places to work with wifi near you!'
+		},
+		locations: [{
+			name: 'Starcups',
+			address: '125 High Street, Reading, RG6 1PS',
+			rating: 3,
+			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+			distance: '100m'
+		},{
+			name: 'Cafe Hero',
+			address: '125 High Street, Reading, RG6 2PS',
+			rating: 4,
+			facilities: ['Premium wifi'],
+			distance: '200m'
+		},{
+			name: 'Burger Queen',
+			address: '125 High Street, Reading, RG6 3PS',
+			rating: 2,
+			facilities: ['Food', 'Premium wifi'],
+			distance: '250m'
+		}]
+	});
+
 }
 /* Get 'Location info' page */
 module.exports.locationInfo = function(req,res){
