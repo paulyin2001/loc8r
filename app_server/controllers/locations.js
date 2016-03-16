edfb1f1a87eab23d9f91cf1bd07881d3f7fa2991
Shih-Chun Yin <scyin0121@gmail.com>
2016-03-15 22:08:59 -0400

    Fix list.jade. Add location info

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -4,7 +4,7 @@ module.exports.homelist = function(req,res){
 }
 /* Get 'Location info' page */
 module.exports.locationInfo = function(req,res){
-	res.render('index', { title: 'Location info'});
+	res.render('location-info', { title: 'Location info'});
 }
 /* Get 'Add review' page */
 module.exports.addReview = function(req,res){
