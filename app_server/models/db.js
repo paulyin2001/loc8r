0f7cbade8893aa4a6f5f71bc3fad3f952dc904fc
Shih-Chun Yin <scyin0121@gmail.com>
2016-03-16 20:06:41 -0400

    Connect to mongoose. Use console log to see the status. install readline for Windows
    Install readline to emulate unix-based system like Linux and Mac for
    SIGINT

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,1 +1,14 @@
 var mongoose = require('mongoose');
+
+var dbURI = 'mongodb://localhost/Loc8r';
+mongoose.connect(dbURI);
+mongoose.connection.on('connected', function(){
+	console.log('Mongoose connected to ' +dbURI);
+	//monitoring for successful connection through Mongoose
+});
+mongoose.connection.on('error', function(err){
+	console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function(){
+	console.log('Mongoose disconnected');
+})
