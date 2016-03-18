20b6d88cb93097e057691d1153ac7c6e7e46af50
Shih-Chun Yin <scyin0121@gmail.com>
2016-03-18 18:42:00 -0400

    add subschema time and review.
    Create db and use command to add data in MongoDB.(NOT showing here)
    mongo
    show dbs
    use Loc8r
    db.locations.save({.......})
    show collections
    db.locations.find().pretty()
    db.locations.update({.......})

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,6 +1,23 @@
 //This file is used to deinfe Mongoose schema
 var mongoose = require( 'mongoose' );
 
+//needs to be in the same file as locationSchema
+//and must be before locationSchema
+var openingTimeSchema = new mongoose.Schema({
+	days: {type: String, required: true},
+	opening: String,
+	closing: String,
+	closed: {type: Boolean, required: true}
+});
+
+//use 'anonymouse' as default
+var reviewSchema = new mongoose.Schema({
+	author: {type: String, "default": 'anonymous'},
+	rating: {type: Number, "default": 0, min: 0, max: 5},
+	createdOn: {type: Date, "default": Date.now},
+	reviewText: String
+});
+
 var locationSchema = new mongoose.Schema({	//constructor func for new schema
 	name: {type: String, required: true},		//validation thru required tag
 	address: String,
@@ -8,10 +25,14 @@ var locationSchema = new mongoose.Schema({	//constructor func for new schema
 	//the default doesn't have to be in quotes, but it is a reserved word in JS
 	//validation with min and max
 	facilities: [String],	//string array
-	coords: {type: [Number], index: '2dsphere'}
+	coords: {type: [Number], index: '2dsphere'},
 	//add GeoJSON path. 2dsphere enables MongoDB to do correct calculations
 	//when running queries and return results. It allows MongoDB to calculate geometries
 	//based on a spherical object.
 	//Coordinate pair must be entered in order: longitude then latitude
+	openingTime: [openingTimeSchema],
+	reviews: [reviewSchema]
 });
 
+//build a model of our location schema. Compile the schema into model
+mongoose.model('Location', locationSchema);
